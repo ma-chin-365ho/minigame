b04b4068b1b77600f18fd27fc2143f45c77d766c
mamemamema <st.msnr.wk@gmail.com>
2025-03-09 06:59:38 +0000

    [Mancala]リファクタリング

---
+++ Mancala/mancala_rule.py
@@ -0,0 +1,67 @@
+
+
+class MancalaRule():
+
+    def __init__(self, cnt_player, cnt_pocket, cnt_start_stone):
+        self.cnt_player = cnt_player
+        self.cnt_pocket = cnt_pocket
+        self.cnt_start_stone = cnt_start_stone
+        self.pocket = [[cnt_start_stone for x in range(cnt_pocket)] for y in range(cnt_player)]
+        self.goal = [0] * cnt_player
+        self.selectable_n_players = [c for c in range(self.cnt_player)]
+        self.selectable_n_pockets = [c for c in range(self.cnt_pocket)]
+        self.winner = None
+        return
+
+    def take_stone(self, n_player, n_pocket):
+        """
+        Returns: 数値: 次のプレイヤーの番号、None: ゲーム終了
+        """
+        cnt_stone = self.pocket[n_player][n_pocket]
+        self.pocket[n_player][n_pocket] = 0
+        pos_player = n_player
+        pos_pocket = n_pocket
+        for c in range(cnt_stone):
+            pos_pocket += 1
+            if pos_pocket == self.cnt_pocket:
+                self.goal[pos_player] +=1
+                if c == cnt_stone - 1:
+                    # ゴールでちょうど終わる時
+                    self.winner = self.__get_winner()
+                    if self.winner is None:
+                        return n_player
+                    else:
+                        return None
+                else:
+                    pos_pocket = -1 # ポジションが最初に戻る。
+                    pos_player = self.__next_player(pos_player)
+            else:          
+                self.pocket[pos_player][pos_pocket] += 1
+        self.winner = self.__get_winner()
+        if self.winner is None:
+            return self.__next_player(n_player)
+        else:
+            return None
+
+    def is_selectable_pocket(self, n_player, n_pocket):
+        if not n_pocket in self.selectable_n_pockets:
+            return False
+        if self.pocket[n_player][n_pocket] == 0:
+            return False
+        return True
+
+    def __next_player(self, now_n_player):
+        next_n_player = now_n_player + 1
+        if next_n_player == self.cnt_player:
+            next_n_player = 0
+        return next_n_player
+
+    def __get_winner(self):
+        """
+        Returns: None: 未確定
+                数値: 勝者のプレイヤー番号
+        """
+        for n_player, pocket_player in enumerate(self.pocket):
+            if all([c == 0 for c in pocket_player]):
+                return n_player
+        return None
--- Mancala/play_mancala.py
+++ Mancala/play_mancala.py
@@ -1,69 +1,25 @@
+from mancala_rule import MancalaRule
+
 CNT_PLAYER = 2
 CNT_POCKET = 3
 CNT_START_STONE = 2
-SELECTABLE_N_PLAYERS = [c for c in range(CNT_PLAYER)]
-SELECTABLE_N_POCKETS = [c for c in range(CNT_POCKET)]
 GAME_STOPPED_CODE = -1
 
-pocket = [[CNT_START_STONE for x in range(CNT_POCKET)] for y in range(CNT_PLAYER)]
-goal = [0] * CNT_PLAYER
-
-def take_stone(n_player, n_pocket):
-    """
-    Returns: True:ターンチェンジする、False:しない
-    """
-    cnt_stone = pocket[n_player][n_pocket]
-    pocket[n_player][n_pocket] = 0
-    pos_player = n_player
-    pos_pocket = n_pocket
-    for c in range(cnt_stone):
-        pos_pocket += 1
-        if pos_pocket == CNT_POCKET:
-            goal[pos_player] +=1
-            if c == cnt_stone - 1:
-                # ゴールでちょうど終わる時
-                return False
-            else:
-                pos_pocket = -1 # ポジションが最初に戻る。
-                pos_player = next_player(pos_player)
-        else:          
-            pocket[pos_player][pos_pocket] += 1
-    return True
-
-def next_player(now_n_player):
-    next_n_player = now_n_player + 1
-    if next_n_player == CNT_PLAYER:
-        next_n_player = 0
-    return next_n_player
-
-def is_selectable_pocket(n_player, n_pocket):
-    if not n_pocket in SELECTABLE_N_POCKETS:
-        return False
-    if pocket[n_player][n_pocket] == 0:
-        return False
-    return True
-
-def get_winner():
-    """
-    Returns: None: 未確定
-             数値: 勝者のプレイヤー番号
-    """
-    for n_player, pocket_player in enumerate(pocket):
-        if all([c == 0 for c in pocket_player]):
-            return n_player
-    return None
-
 def play_mancala_stdio():
+    mancala_rule = MancalaRule(
+        CNT_PLAYER, CNT_POCKET, CNT_START_STONE
+    )
+    
     print("========================================")
     print("Mancala Start !!")
     print("  (Game Stop " + str([GAME_STOPPED_CODE]) + ")")
     print("========================================")
     while True:
         try:
-            turn_n_player = int(input("Select Start Player " + str(SELECTABLE_N_PLAYERS) + ": "))
+            turn_n_player = int(input("Select Start Player " + str(mancala_rule.selectable_n_players) + ": "))
             if turn_n_player == GAME_STOPPED_CODE:
                 return
-            if not turn_n_player in SELECTABLE_N_PLAYERS:
+            if not turn_n_player in mancala_rule.selectable_n_players:
                 raise Exception("Out of Selectable Players")
             break
         except:
@@ -71,33 +27,29 @@ def play_mancala_stdio():
 
     while True:
         print("========================================")
-        print("Pocket: " + str(pocket))
-        print("Goal: " + str(goal))
+        print("Pocket: " + str(mancala_rule.pocket))
+        print("Goal: " + str(mancala_rule.goal))
         print("Turn: " + str(turn_n_player))
         print("========================================")
 
         while True:
             try:
-                select_n_pocket = int(input("Select Pocket " + str(SELECTABLE_N_POCKETS) + ": "))
+                select_n_pocket = int(input("Select Pocket " + str(mancala_rule.selectable_n_pockets) + ": "))
                 if select_n_pocket == GAME_STOPPED_CODE:
                     return
-                if not is_selectable_pocket(turn_n_player, select_n_pocket):
+                if not mancala_rule.is_selectable_pocket(turn_n_player, select_n_pocket):
                     raise Exception("Out of Selectable Pockets")
                 break
             except:
                 pass
 
-        is_next_player = take_stone(turn_n_player, select_n_pocket)
-        winner = get_winner()
-        if winner is not None:
+        turn_n_player = mancala_rule.take_stone(turn_n_player, select_n_pocket)
+        if turn_n_player is None:
             print("")
             print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             print("Game Over!!!")
-            print("Winner Player: " + str(turn_n_player))
+            print("Winner Player: " + str(mancala_rule.winner))
             break
-        else:
-            if is_next_player:
-                turn_n_player = next_player(turn_n_player)
 
 if __name__ == "__main__":
     play_mancala_stdio()
